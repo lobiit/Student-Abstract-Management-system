cd6fcc373df207baeb794a3111d299dc4cd1bddd
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-26 15:21:35 +0300

    total room count functionality

--- base/views.py
+++ base/views.py
@@ -22,7 +22,8 @@ def home(request):
         Q(description__icontains=q)
     )
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'home.html', context)
 
 
