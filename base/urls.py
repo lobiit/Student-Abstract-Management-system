0ad33376f818dd72df4c34d62acbe387cbacc4b6
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-26 11:43:35 +0300

    added the update room functionality

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -34,3 +34,15 @@ def create_room(request):
             return redirect('home')
     context = {'form': form}
     return render(request, "room_form.html", context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, "room_form.html", context)
