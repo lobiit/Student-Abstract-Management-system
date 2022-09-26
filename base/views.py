0d5978ab079db51e4ea7178b0a5239664c858da3
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-26 13:09:02 +0300

    added search by room functionality

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 """
@@ -14,8 +14,10 @@ rooms = [
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get("q") if request.GET.get("q") is not None else ""
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'home.html', context)
 
 
