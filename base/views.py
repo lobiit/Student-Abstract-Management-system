d7d9ee1fdfce304b42db2970446b952c0116ebd4
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-27 11:10:57 +0300

    created delete comment functionality

--- base/views.py
+++ base/views.py
@@ -130,3 +130,14 @@ def delete_room(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "delete.html", {'obj': room})
+
+
+@login_required(login_url='login')
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("You are not allowed to edit here!!")
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "delete.html", {'obj': message})
