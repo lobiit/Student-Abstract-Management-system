755ad6e35b15d8aa641fae12fdf793a4f421f308
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-27 10:54:17 +0300

    Added participants section

--- base/views.py
+++ base/views.py
@@ -79,14 +79,16 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created_at')
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, "room.html", context)
 
 
